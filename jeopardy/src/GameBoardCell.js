380a481dae8ece492879a850fb10644d24d855e2
jlegreid <jmorgan@coschedule.com>
2020-09-07 01:40:46 -0500

    Style refinements to gameboard

--- jeopardy/src/GameBoardCell.js
+++ jeopardy/src/GameBoardCell.js
@@ -3,40 +3,56 @@ import Modal from './Modal';
 
 function GameBoardCell({ onClose, ...props }) { 
     const [isModalOpen, setIsModalOpen] = useState(false);
-    const [showAnswer, setShowAnswer] = useState('');
+    const [showAnswer, setShowAnswer] = useState(null);
     const [clueViewed, setClueViewed] = useState(false);
     const handleClueClick = () => {
         setIsModalOpen(true)
         setClueViewed(true);
     }
+    const handleOnClose = () => {
+        setIsModalOpen(false)
+        // setShowAnswer(null)
+    }
 
+    const question = () => {
+        return props.clue.question.replace( /(<([^>]+)>)/ig, '').replace(/[\\]/g, '');;
+    }
+    const answer = () => {
+        return props.clue.answer.replace( /(<([^>]+)>)/ig, '').replace(/[\\]/g, '');;
+    }
+    
     if (props.type === 'header') {
         return (
-            <div className='game-board-category-header-cell'>
-                <span id={props.category.id}>{props.category.title}</span>
+            <div className="game-board-category-header-cell">
+                <div className="game-board-category-header-cell-button" id={props.category.id}><span>{props.category.title}</span></div>
             </div>
         )
     } else if (props.type === 'body') {
         return (
-            <div className='game-board-category-body-cell' clue={props.clue}>
+            <div className='game-board-category-body-cell'>
                 <button 
                     id={props.clue.id} 
+                    className="game-board-category-body-cell-button"
                     category={props.clue.category_id} 
                     onClick={handleClueClick}>
                     {clueViewed ? '' : <span>${props.clue.value}</span>}
                 </button>
                 {isModalOpen && (
-                    <Modal classes="clue-modal" onClose={() => setIsModalOpen(false)}>
+                    <Modal classes="clue-modal">
                         <div className={"modal-body " + showAnswer}>
                             <div className="clue-card answer-side">
-                                <span>{props.clue.answer}</span>
-                                <button onClick={() => setShowAnswer(null)}>Show Question</button>
-                                <button className="action-button secondary" onClick={onClose}>Done</button>
+                                <span className="clue-text">{answer()}</span>
+                                <div className="clue-card-actions">
+                                    <button className="action-button" onClick={() => setShowAnswer('show-question')}>Show Question</button>
+                                    <button className="action-button secondary" onClick={handleOnClose}>Done</button>
+                                </div>
                             </div>
                             <div className="clue-card question-side">
-                                <span>{props.clue.question}</span>
-                                <button onClick={() => setShowAnswer('show-answer')}>Show Answer</button>
-                                <button className="action-button secondary" onClick={onClose}>Done</button>
+                                <span className="clue-text">{question()}</span>
+                                <div className="clue-card-actions">
+                                    <button className="action-button" onClick={() => setShowAnswer('show-answer')}>Show Answer</button>
+                                    <button className="action-button secondary" onClick={handleOnClose}>Done</button>
+                                </div>
                             </div>
                         </div>
                     </Modal>
